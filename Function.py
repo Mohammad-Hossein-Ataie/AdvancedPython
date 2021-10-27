243e8d97b31efb2a9cccbe69e5f812d2521310ea
Mohammad Hossein Ataie <48953110+Mohammad-Hossein-Ataie@users.noreply.github.com>
2021-10-27 22:27:41 +0330

    Add files via upload

---
+++ Function.py
@@ -0,0 +1,13 @@
+def greet(firstName, lastName):
+    print(f"Hi {firstName} {lastName}")
+
+
+greet('ali', 'salehi')
+
+
+def getGreeting(name):
+    return f"Hi {name}"
+
+
+message = getGreeting("saye")
+# All function by defualt return None unless you return something.
